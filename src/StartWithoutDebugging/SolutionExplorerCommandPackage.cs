da54fed1f3ef0b6e0a61da5556fe530240239c7a
vurdalakov <vurdalakov@gmail.com>
2018-03-15 10:44:31 +0200

    Version 1.1, minor fixes

--- src/StartWithoutDebugging/SolutionExplorerCommandPackage.cs
+++ src/StartWithoutDebugging/SolutionExplorerCommandPackage.cs
@@ -30,7 +30,7 @@ namespace StartWithoutDebugging
     /// </para>
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true)]
-    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [InstalledProductRegistration("#110", "#112", "1.1", IconResourceID = 400)] // Info on this package for Help/About
     [ProvideMenuResource("Menus.ctmenu", 1)]
     [Guid(SolutionExplorerCommandPackage.PackageGuidString)]
     [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
